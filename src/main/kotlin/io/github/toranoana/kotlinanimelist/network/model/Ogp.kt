07793b4aa1710c5b9998ad814440fbe03899ab82
KarageAgeta <yoko.karasaki@gmail.com>
2018-03-29 16:56:05 +0900

    [fix] Add comment
    - Add comment

--- src/main/kotlin/io/github/toranoana/kotlinanimelist/network/model/Ogp.kt
+++ src/main/kotlin/io/github/toranoana/kotlinanimelist/network/model/Ogp.kt
@@ -1,5 +1,6 @@
 package io.github.toranoana.kotlinanimelist.network.model
 
 data class Ogp(
-    val og_image: String = ""
+    // TODO : API から取得する項目を追加
+    val og_image: String = "http://placehold.jp/50/e0e0e0/424242/600x400.png?text=NO%20IMAGE"
 )
